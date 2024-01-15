d182b032bbf5d46a82535c7b833d0cd6eee4e753
YutaAdachi <yuta.adachi@luxiar.com>
2024-01-15 16:10:24 +0900

    MoveValidatorクラスの実装

--- game.rb
+++ game.rb
@@ -11,6 +11,7 @@ class Game
     @current_player = @player_1
     @board = Board.new(@player_1, @player_2)
     @display = Display.new(@board, @player_1, @player_2)
+    @move_validator = MoveValidator.new(@board)
   end
 
   def play
@@ -23,12 +24,12 @@ class Game
       if input_2
         move_from = Input.parse(input_1)
         move_to = Input.parse(input_2)
-        next unless valid_move?(move_from, move_to)
+        next unless move_validator.valid_move?(move_from, move_to)
 
         game_over = @board.move_piece(move_from, move_to, @current_player)
       else
         move_to = Input.parse(input_1)
-        next unless valid_place?(move_to)
+        next unless move_validator.valid_place?(move_to)
 
         game_over = @board.place_from_captured_pieces(move_to, @current_player)
       end
@@ -40,44 +41,6 @@ class Game
   end
 
   private
-
-  # 駒が動かせる場所かどうか
-  def valid_move?(move_from, move_to)
-    piece = @board.board[move_from.x][move_from.y]
-    target_place = @board.board[move_to.x][move_to.y]
-
-    if piece.nil?
-      puts '駒がありません'
-
-      false
-    elsif piece.player != @current_player
-      puts '自分の駒を選択してください' 
-      
-      false
-    elsif target_place&.player == @current_player
-      puts '自分の駒があります' 
-      
-      false
-    elsif !piece.possible_moves(move_from).include?([move_to.x, move_to.y])
-      puts 'そこには動かせません'
-    else
-      true
-    end
-  end
-
-  def valid_place?(move_to)
-    if @board.board[move_to.x][move_to.y]
-      puts '駒があります'
-
-      false
-    elsif @current_player.search_piece(move_to.piece).nil?
-      puts '入力された駒を持っていません'
-
-      false
-    else
-      true
-    end
-  end
 
   def change_turns
     @current_player = @current_player == @player_1 ? @player_2 : @player_1
