b0fc80d092e4394f506b41b50fa5b750ebcdacdd
YutaAdachi <yuta.adachi@luxiar.com>
2024-01-15 18:09:50 +0900

    ワークフローテスト

--- game.rb
+++ game.rb
@@ -45,6 +45,30 @@ class Game
     end
   end
 
+  # 駒が動かせる場所かどうか
+  def valid_move?(move_from, move_to)
+    piece = @board.board[move_from.x][move_from.y]
+    target_place = @board.board[move_to.x][move_to.y]
+
+    if piece.nil?
+      puts '駒がありません'
+
+      false
+    elsif piece.player != @current_player
+      puts '自分の駒を選択してください' 
+      
+      false
+    elsif target_place&.player == @current_player
+      puts '自分の駒があります' 
+      
+      false
+    elsif !piece.possible_moves(move_from).include?([move_to.x, move_to.y])
+      puts 'そこには動かせません'
+    else
+      true
+    end
+  end
+
   private
 
   def change_turns
